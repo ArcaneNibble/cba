44d431e16f9bcb4e802ff7dcc3641be8cf8a50e5
R <rqou@berkeley.edu>
2022-03-08 02:17:28 -0800

    simple test reads; unverified

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,19 @@ impl AIGRef {
     pub const fn is_invert(&self) -> bool {
         self.0 & Self::FLG_INVERT != 0
     }
+    pub fn set_invert(&mut self, invert: bool) {
+        if invert {
+            self.0 |= Self::FLG_INVERT;
+        } else {
+            self.0 &= !Self::FLG_INVERT;
+        }
+    }
+    pub const fn inv(self) -> Self {
+        Self(self.0 | Self::FLG_INVERT)
+    }
+    pub const fn no_inv(self) -> Self {
+        Self(self.0 & !Self::FLG_INVERT)
+    }
 
     pub const fn idx(&self) -> usize {
         debug_assert!(!self.is_pi());
@@ -58,6 +71,18 @@ pub struct AIGNode {
     /// number of fanouts in the resulting LUT map
     /// will be abused for initial topological order computation as a mark flag
     pub num_fanouts: u32,
+}
+
+impl AIGNode {
+    pub fn new(inp0: AIGRef, inp1: AIGRef, name: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            inp0,
+            inp1,
+
+            num_fanouts: 0,
+        }
+    }
 }
 
 /// Complete AIG, including inputs and outputs
@@ -87,11 +112,13 @@ impl AIG {
         }
         let top_module = top_module.unwrap();
 
+        #[derive(Debug)]
         struct NetAttrs<'a> {
             name: Option<&'a str>,
             driver: Option<&'a Cell>,
+            input: bool,
         }
-        let mut nets: HashMap<usize, NetAttrs> = HashMap::new();
+        let mut nets = HashMap::new();
 
         // read netnames
         for (netname_str, netname_obj) in top_module.netnames.iter() {
@@ -99,6 +126,7 @@ impl AIG {
             let attr = NetAttrs {
                 name: Some(netname_str),
                 driver: None,
+                input: false,
             };
             let bitidx = match netname_obj.bits[0] {
                 BitVal::N(n) => n,
@@ -108,11 +136,47 @@ impl AIG {
             nets.insert(bitidx, attr);
         }
 
-        let nodes = Vec::new();
-        let mut pi = Vec::new();
-        let po = Vec::new();
+        // read cells
+        fn get_cell_conn(cell: &Cell, conn: &str) -> usize {
+            let output = cell
+                .connections
+                .get(conn)
+                .expect(&format!("missing {} connection", conn));
+            assert_eq!(output.len(), 1);
+            match output[0] {
+                BitVal::N(n) => n,
+                BitVal::S(_) => panic!("can't be a constant"),
+            }
+        }
 
-        // read ports
+        for (cellname, cellobj) in top_module.cells.iter() {
+            println!("cell {} of type {}", cellname, cellobj.cell_type);
+
+            let output = get_cell_conn(cellobj, "Y");
+            println!("output wire is {}", output);
+
+            if let Some(attr) = nets.get_mut(&output) {
+                assert!(attr.driver.is_none());
+                attr.driver = Some(cellobj);
+            } else {
+                let attr = NetAttrs {
+                    name: None,
+                    driver: Some(cellobj),
+                    input: false,
+                };
+                nets.insert(output, attr);
+            }
+        }
+
+        println!("nets {:#?}", nets);
+
+        let mut nodes = Vec::new();
+        let mut pi = Vec::new();
+        let mut po = Vec::new();
+
+        let mut pimap = HashMap::new();
+
+        // read input ports
         for (portname, portobj) in top_module.ports.iter() {
             assert_eq!(portobj.bits.len(), 1);
             let bitidx = match portobj.bits[0] {
@@ -120,29 +184,112 @@ impl AIG {
                 BitVal::S(_) => panic!("can't be a constant"),
             };
 
-            if let Some(netattr) = nets.get(&bitidx) {
-                assert_eq!(netattr.name.unwrap(), portname);
-            } else {
-                let attr = NetAttrs {
-                    name: Some(portname),
-                    driver: None,
-                };
-                nets.insert(bitidx, attr);
-            }
-
             match portobj.direction {
                 PortDirection::Input => {
+                    if let Some(netattr) = nets.get_mut(&bitidx) {
+                        assert_eq!(netattr.name.unwrap(), portname);
+                        assert!(netattr.driver.is_none());
+                        netattr.input = true;
+                    } else {
+                        let attr = NetAttrs {
+                            name: Some(portname),
+                            driver: None,
+                            input: true,
+                        };
+                        nets.insert(bitidx, attr);
+                    }
+
                     let pi_idx = pi.len();
                     println!(
                         "input bit {} is called {} --> pi idx {}",
                         bitidx, portname, pi_idx
                     );
                     pi.push(portname.clone());
+                    pimap.insert(bitidx, pi_idx);
                 }
                 PortDirection::Output => {
                     // purposely don't do anything here yet
                 }
                 PortDirection::InOut => panic!("can't be inout"),
+            }
+        }
+
+        fn process_input(
+            nodes: &mut Vec<AIGNode>,
+            nets: &HashMap<usize, NetAttrs>,
+            pimap: &HashMap<usize, usize>,
+            yosys_net_idx: usize,
+        ) -> AIGRef {
+            let net = nets.get(&yosys_net_idx).unwrap();
+            println!("processing net {} -> {:?}", yosys_net_idx, net);
+            if net.input {
+                let pi_idx = pimap.get(&yosys_net_idx).unwrap();
+                AIGRef::new(*pi_idx, false, true)
+            } else {
+                process_cell(
+                    nodes,
+                    nets,
+                    pimap,
+                    net.name.unwrap_or(""),
+                    net.driver.unwrap(),
+                )
+            }
+        }
+
+        fn process_cell(
+            nodes: &mut Vec<AIGNode>,
+            nets: &HashMap<usize, NetAttrs>,
+            pimap: &HashMap<usize, usize>,
+            cellname: &str,
+            cell: &Cell,
+        ) -> AIGRef {
+            fn add_cell(nodes: &mut Vec<AIGNode>, n: AIGNode) -> AIGRef {
+                let cell_idx = nodes.len();
+                println!("adding AIG cell {:?} @ {}", n, cell_idx);
+                nodes.push(n);
+                AIGRef::new(cell_idx, false, false)
+            }
+
+            match &cell.cell_type as &str {
+                "$_AND_" => {
+                    let a = process_input(nodes, nets, pimap, get_cell_conn(cell, "A"));
+                    let b = process_input(nodes, nets, pimap, get_cell_conn(cell, "B"));
+
+                    add_cell(nodes, AIGNode::new(a, b, cellname))
+                }
+                "$_XOR_" => {
+                    let a = process_input(nodes, nets, pimap, get_cell_conn(cell, "A"));
+                    let b = process_input(nodes, nets, pimap, get_cell_conn(cell, "B"));
+
+                    let n1 = add_cell(nodes, AIGNode::new(a, b.inv(), &format!("{}_n1", cellname)));
+                    let n2 = add_cell(nodes, AIGNode::new(a.inv(), b, &format!("{}_n2", cellname)));
+                    let n3 = add_cell(
+                        nodes,
+                        AIGNode::new(n1.inv(), n2.inv(), &format!("{}_n3", cellname)),
+                    );
+                    n3.inv()
+                }
+                _ => panic!("unknown cell type {}", cell.cell_type),
+            }
+        }
+
+        // read output ports
+        for (portname, portobj) in top_module.ports.iter() {
+            assert_eq!(portobj.bits.len(), 1);
+            let bitidx = match portobj.bits[0] {
+                BitVal::N(n) => n,
+                BitVal::S(_) => panic!("can't be a constant"),
+            };
+
+            if portobj.direction == PortDirection::Output {
+                let net = nets.get(&bitidx).expect("output referenced invalid bit");
+                assert!(!net.input);
+                assert_eq!(net.name.unwrap(), portname);
+                let cell = net.driver.expect("undriven net");
+
+                println!("output {} is from cell {:#?}", portname, cell);
+                let po_aigref = process_cell(&mut nodes, &nets, &pimap, portname, cell);
+                po.push((portname.clone(), po_aigref));
             }
         }
 
@@ -161,8 +308,19 @@ impl AIG {
             .unwrap();
 
         for pi in &self.pi {
-            f.write(format!("{} [label=\"{}\nPI\"];\n", pi, pi).as_bytes())
+            f.write(format!("{} [shape=triangle label=\"{}\nPI\"];\n", pi, pi).as_bytes())
                 .unwrap();
+        }
+
+        for (po_name, _po_ref) in &self.po {
+            f.write(
+                format!(
+                    "{} [shape=invtriangle label=\"{}\nPO\"];\n",
+                    po_name, po_name
+                )
+                .as_bytes(),
+            )
+            .unwrap();
         }
 
         f.write("}\n".as_bytes()).unwrap();
@@ -183,5 +341,6 @@ fn main() {
 
     // Make AIG
     let aig = AIG::parse_netlist(netlist);
+    println!("final AIG is {:#?}", aig);
     aig.dump_dot("nodes");
 }
